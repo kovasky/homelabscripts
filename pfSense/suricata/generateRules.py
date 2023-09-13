eb435cc0f4b48682d4d37cebd273d27e0fbc5523
kovasky <kab310@mun.ca>
2023-09-12 21:24:22 -0400

    naming convention

--- pfSense/suricata/generateRules.py
+++ pfSense/suricata/generateRules.py
@@ -20,8 +20,8 @@ def generateRule(dnsName, currentSID, outputFile) -> None:
         outputFile.write(outboundRule)
         print(f"Converted {dnsName} to {ipAddr} and added outbound rule with SID {currentSID}")
         currentSID += 1
-        inbound_rule = f'pass ip {ipAddr} any -> any any (msg:"Allow inbound connection from {ipAddr}"; sid:{currentSID};)\n'
-        outputFile.write(inbound_rule)
+        inboundRule = f'pass ip {ipAddr} any -> any any (msg:"Allow inbound connection from {ipAddr}"; sid:{currentSID};)\n'
+        outputFile.write(inboundRule)
         print(f"Added inbound rule for {ipAddr} with SID {currentSID}")
         currentSID += 1
 
@@ -33,11 +33,11 @@ if __name__ == "__main__":
     inputFilePath = sys.argv[1]
     outputFilePath = sys.argv[2]
 
-    with open(inputFilePath, "r") as input_file, open(outputFilePath, "w") as outputFile:
+    with open(inputFilePath, "r") as inputFile, open(outputFilePath, "w") as outputFile:
         currentSID = 1
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
             futures = []
-            for line in input_file:
+            for line in inputFile:
                 future = executor.submit(generateRule, line, currentSID, outputFile)
                 futures.append(future)
                 currentSID += 2
